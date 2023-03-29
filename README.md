e18f9dd4610f1b43e9a34fd06a33f3a3afc44da7
Andrew Brock <arbrock@sq1t.com>
2023-03-29 12:03:39 -0500

    Second attempt: no longer using NAT

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # conduit-iocage
 
 An iocage integration for conduit.rs, so I can run it on TrueNAS.
+
+This is not even alpha-quality software: don't even think about using it.
--- post_install.sh
+++ post_install.sh
@@ -1,7 +1,32 @@
 #!/bin/sh
 
-pw add user -n conduit -C Conduit -s /bin/nologin -m
-
 sysrc conduit_enable="YES"
+sysrc nginx_enable="YES"
 
-service conduit start
+SERVER_NAME=your.server.name
+ed /usr/local/etc/conduit.toml << EOF
+/server_name/
+o
+server_name = "$SERVER_NAME"
+.
+wq
+EOF
+ed /usr/local/nginx/nginx.conf << EOF
+$
+-1
+i
+    server {
+        listen      443 ssl;
+        server_name $SERVER_NAME;
+        location /_matrix/ {
+          proxy_pass http://localhost:6167/;
+          include proxy_params;
+        }
+    }
+.
+wq
+EOF
+
+echo "You will need to set server_name in /usr/local/etc/conduit.toml and then run
+
+service conduit start"
