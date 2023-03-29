09a76b3b6519bb5e224e9db504a7ba3339c87318
Andrew Brock <arbrock@sq1t.com>
2023-03-29 13:11:25 -0500

    Nginx proxy parameters from conduit docs

--- post_install.sh
+++ post_install.sh
@@ -9,7 +9,7 @@ if [ -z "$SERVER_NAME" ] ; then
   SERVER_NAME=your.server.name
 fi
 ed /usr/local/etc/conduit.toml << EOF
-/server_name/
+/NEED TO EDIT/
 i
 server_name = "$SERVER_NAME"
 .
@@ -20,19 +20,25 @@ $
 -1
 i
     server {
-        listen      443 ssl;
+        listen      443 ssl http2;
+        listen      8448 ssh http2;
+        listen      [::]:443 ssl http2;
+        listen      [::]:8448 ssl http2;
         server_name $SERVER_NAME;
 
-        ssl_certificate           cert.pem
-        ssl_certificat_key        cert.key
+        ssl_certificate           cert.pem;
+        ssl_certificate_key       cert.key;
         ssl_session_cache         shared:SSL:1m;
         ssl_session_timeout       5m;
         ssl_ciphers               HIGH:!aNULL:!MD5;
         ssl_prefer_server_ciphers on;
 
+        client_max_body_size 20M;
+
         location /_matrix/ {
-          proxy_pass http://localhost:6167/;
-          include proxy_params;
+          proxy_pass http://localhost:6167$request_uri;
+          proxy_set_header Host $http_host;
+          proxy_buffering off;
         }
     }
 .
